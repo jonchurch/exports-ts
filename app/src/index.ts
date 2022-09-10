80f4f915db049063380d146afdfae2f2d1e5c551
Jon Church <jonchurch@microsoft.com>
2022-09-10 01:00:20 -0400

    get submodule to work but break bare import

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,9 +1,10 @@
 import { exit } from "node:process";
+
 import { wait } from "package";
 import { doNotWait } from "package/submodule";
 
 const world = "hello";
 
 wait(1000).then(() => console.log(world));
-doNotWait();
+doNotWait(1);
 exit(0);
